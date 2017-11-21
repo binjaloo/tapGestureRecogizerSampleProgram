69af6ac89c6409758cdccae7db3a6b0e935cad74
binjaloo <rommel@binjaloo.com>
2017-11-21 11:56:41 -0800

    Initial Commit

---
+++ tapGestureRecogizerSampleProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tapGestureRecogizerSampleProgram
+//
+//  Created by KLabs on 11/21/17.
+//  Copyright © 2017 KLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tapGestureRecogizerSampleProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tapGestureRecogizerSampleProgram
+//
+//  Created by KLabs on 11/21/17.
+//  Copyright © 2017 KLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
